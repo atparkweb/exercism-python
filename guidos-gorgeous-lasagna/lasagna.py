03294f00fcae34a3efafd5288c19038d7950d0c3
Andrew Park <atpark333@gmail.com>
2022-05-30 00:28:55 +0900

    Remove unnecessary parens

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -15,7 +15,7 @@ def bake_time_remaining(elapsed_bake_time: int):
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
-    return (EXPECTED_BAKE_TIME - elapsed_bake_time)
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
 def preparation_time_in_minutes(number_of_layers: int):
