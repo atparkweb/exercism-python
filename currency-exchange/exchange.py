34580a150e319807b1880e387c22071ef2e34fe2
Andrew Park <atpark333@gmail.com>
2022-05-30 22:15:35 +0900

    Implement exchange.change_value function

--- currency-exchange/exchange.py
+++ currency-exchange/exchange.py
@@ -6,29 +6,32 @@ def exchange_money(budget, exchange_rate):
     :param exchange_rate: float - unit value of the foreign currency.
     :return: float - exchanged value of the foreign currency you can receive.
     """
+
     return budget / exchange_rate
 
 
 def get_change(budget, exchanging_value):
     """
+    Calculate currency left after an exchange
 
     :param budget: float - amount of money you own.
     :param exchanging_value: int - amount of your money you want to exchange now.
     :return: float - amount left of your starting currency after exchanging.
     """
 
-    pass
+    return budget - exchanging_value
 
 
 def get_value_of_bills(denomination, number_of_bills):
     """
+    Calculate value of bills
 
     :param denomination: int - the value of a bill.
     :param number_of_bills: int - amount of bills you received.
     :return: int - total value of bills you now have.
     """
 
-    pass
+    return int(denomination * number_of_bills)
 
 
 def get_number_of_bills(budget, denomination):
