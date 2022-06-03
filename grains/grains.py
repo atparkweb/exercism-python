f09abb152d75b911fc6a70dea19f4715b263ddba
Andrew Park <atpark333@gmail.com>
2022-06-03 11:52:32 +0900

    Add initial quick implementation of grains.square

--- grains/grains.py
+++ grains/grains.py
@@ -1,5 +1,12 @@
+import math
+
 def square(number):
-    pass
+    if number > 64 or number < 1:
+        raise ValueError('square must be between 1 and 64')
+
+    if number == 1: return 1
+
+    return square(number - 1) * 2
 
 
 def total():
