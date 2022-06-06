1c932ee387afa11889c598586f60e006e7050dfe
Andrew Park <atpark333@gmail.com>
2022-06-07 00:09:46 +0900

    Fix whitespace

--- grains/grains.py
+++ grains/grains.py
@@ -1,17 +1,15 @@
 import math
 
+
 def square(number):
     if number > 64 or number < 1:
         raise ValueError('square must be between 1 and 64')
-
     return 2 ** (number - 1)
 
 
 def total():
     result = 0
-
     squares = range(1, 65)
     for value in squares:
         result += square(value)
-
     return result
